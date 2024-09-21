95cd044c8e683c6d2fb95831fc0b1b9e12c01384
Alessandro Ribeiro <alessandro.ribeiro.silva@gmail.com>
2024-09-20 23:14:31 -0300

    fix: imguizmos not enabling the guizmo highlight to edit

--- src/imgui_editor/ImGui/View/Scene.h
+++ src/imgui_editor/ImGui/View/Scene.h
@@ -9,6 +9,12 @@ class Scene : public View
 public:
     static const ViewType Type;
 
+    ImGuizmo::OPERATION mCurrentGizmoOperation;// = ImGuizmo::TRANSLATE;
+    ImGuizmo::MODE mCurrentGizmoMode;// = ImGuizmo::LOCAL;
+
+    bool useSnap;// = true;
+    float snap[3];// = {0.01f,0.01f,0.01f};
+
     EventCore::Callback<void(const ImVec2 &pos, const ImVec2 &size)> OnImGuiDrawOverlay;
 
     Scene();
