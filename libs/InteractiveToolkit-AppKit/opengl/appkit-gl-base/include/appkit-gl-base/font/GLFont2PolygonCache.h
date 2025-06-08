7943077f93e374054435e9336ffbea87cf5a1093
Alessandro Ribeiro <alessandro.ribeiro.silva@gmail.com>
2025-06-07 22:01:47 -0300

    add GLFont2PolygonCache for caching polygon glyphs and triangulation

---
+++ libs/InteractiveToolkit-AppKit/opengl/appkit-gl-base/include/appkit-gl-base/font/GLFont2PolygonCache.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "GLFont2.h"
+#include <InteractiveToolkit/AlgorithmCore/Polygon/Polygon2D.h>
+#include <InteractiveToolkit/AlgorithmCore/Polygon/TriangulatorEarClipping.h>
+
+namespace AppKit
+{
+    namespace OpenGL
+    {
+        struct GLFont2TriangulatedGlyph
+        {
+            MathCore::vec3f advancex;
+            std::vector<MathCore::vec3f> vertices;
+            std::vector<uint32_t> triangles; ///< Indices to the vertices that form triangles
+        };
+
+        /// \brief A cache for polygon glyphs
+        ///
+        /// This class is used to cache polygon glyphs for a font.
+        ///
+        /// \author Alessandro Ribeiro
+        ///
+        class GLFont2PolygonCache
+        {
+        public:
+            std::unordered_map<uint32_t, GLFont2TriangulatedGlyph> triangulated_glyphs;
+
+            GLFont2PolygonCache() = default;
+            GLFont2PolygonCache(const GLFont2PolygonCache &other) = default;
+            GLFont2PolygonCache(GLFont2PolygonCache &&other) noexcept = default;
+            GLFont2PolygonCache &operator=(const GLFont2PolygonCache &other) = default;
+            GLFont2PolygonCache &operator=(GLFont2PolygonCache &&other) noexcept = default;
+
+            void setFromGLFont2(GLFont2 &glFont2, float size, float max_distance_tolerance);
+        };
+
+    } // namespace OpenGL
+
+} // namespace AppKit
