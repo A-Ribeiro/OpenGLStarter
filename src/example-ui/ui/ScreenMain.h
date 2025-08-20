9ba1b6213dabcf8fbce9f6c77a816445e174caf9
Alessandro Ribeiro <alessandro.ribeiro.silva@gmail.com>
2025-08-20 12:01:12 -0300

    Implement layout and button management in ScreenMain; enhance ScreenManager event handling

--- src/example-ui/ui/ScreenMain.h
+++ src/example-ui/ui/ScreenMain.h
@@ -6,7 +6,27 @@ namespace ui
 {
     class ScreenMain : public ui::Screen
     {
+        void layoutElements(const MathCore::vec2i &size);
+        void addButton(const std::string &text);
+        void previousButton();
+        void nextButton();
+        
+        bool change_screen;
+        float increase_speed_for_secs_and_trigger_action;
+        float osciloscope;
+        int selected_button;
+        ui::ScreenManager *screenManager;
     public:
+
+        static constexpr float width = 256;
+        static constexpr float height = 64;
+        static constexpr float gap = 10;
+
+        static constexpr float margin_bottom = 64;
+
+        static constexpr float osciloscope_normal_hz = 1.0f;
+        static constexpr float osciloscope_selected_hz = 6.0f;
+
         std::shared_ptr<AppKit::GLEngine::Components::ComponentUI> uiComponent;
         std::shared_ptr<AppKit::GLEngine::Transform> uiNode;
 
