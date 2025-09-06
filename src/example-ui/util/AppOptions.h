830d8e5021f90b1107041a2a99ff761f9679e345
Alessandro Ribeiro <alessandro.ribeiro.silva@gmail.com>
2025-09-05 21:13:04 -0300

    added DPI support and refactored AppOptions for improved options management and saving/loading functionality

--- src/example-ui/util/AppOptions.h
+++ src/example-ui/util/AppOptions.h
@@ -17,15 +17,12 @@ namespace AppOptions
             char Input[64];
             char Movement[64];
         } Control;
-        static constexpr const char *ControlInput[3] = {"Steam 1", "Keyboard", "Steam 1 + Keyboard"};
-        static constexpr const char *ControlMovement[2] = {"Fluid", "Legacy"};
 
         struct
         {
             char EffectsVolume[64];
             char MusicVolume[64];
         } Audio;
-        static constexpr const char *AudioVolume[11] = {"0", "10", "20", "30", "40", "50", "60", "70", "80", "90", "100"};
 
         struct
         {
@@ -35,11 +32,6 @@ namespace AppOptions
             char AntiAliasing[64];
             char VSync[64];
         } Video;
-        static constexpr const char *VideoWindowMode[3] = {"Window", "Borderless", "Fullscreen"};
-        // static constexpr const char *VideoResolution[4] = {"2560x1440", "1920x1080", "1280x720", "854x480"};
-        static constexpr const char *VideoAspect[2] = {"16:9", "16:10"};
-        static constexpr const char *VideoAntiAliasing[2] = {"MSAA", "OFF"};
-        static constexpr const char *VideoVSync[2] = {"ON", "OFF"};
 
         struct
         {
@@ -49,23 +41,58 @@ namespace AppOptions
             char Particles[64];
             char OnGameStats[64];
         } Extra;
+    };
+
+    namespace OptionsConstants
+    {
+        static constexpr const char *ControlInput[3] = {"Steam 1", "Keyboard", "Steam 1 + Keyboard"};
+        static constexpr const char *ControlMovement[2] = {"Fluid", "Legacy"};
+
+        static constexpr const char *AudioVolume[11] = {"0", "10", "20", "30", "40", "50", "60", "70", "80", "90", "100"};
+
+        static constexpr const char *VideoWindowMode[3] = {"Window", "Borderless", "Fullscreen"};
+        // static constexpr const char *VideoResolution[4] = {"2560x1440", "1920x1080", "1280x720", "854x480"};
+        static constexpr const char *VideoAspect[2] = {"16:9", "16:10"};
+        static constexpr const char *VideoAntiAliasing[2] = {"MSAA", "OFF"};
+        static constexpr const char *VideoVSync[2] = {"ON", "OFF"};
+
         static constexpr const char *ExtraLanguage[2] = {"English", "Português (BR)"};
         static constexpr const char *ExtraColorScheme[4] = {"Purple", "Orange", "Green", "Blue"};
         static constexpr const char *ExtraMeshCrusher[2] = {"ON", "OFF"};
         static constexpr const char *ExtraParticles[4] = {"Low", "Medium", "High", "Ultra"};
         static constexpr const char *ExtraOnGameStats[2] = {"OFF", "FPS"};
+
+        static constexpr const char *groups[4] = {"Control", "Audio", "Video", "Extra"};
+
     };
 
-    void generate_binary_data_v1(const AppOptions_v1 &options, Platform::ObjectBuffer *output);
-    bool parse_binary_data_v1(const Platform::ObjectBuffer &data, AppOptions_v1 *out_options);
-    void create_binary_data_v1(AppOptions_v1 *options);
-
+    void write_binary_data_v1(const AppOptions_v1 &options, Platform::ObjectBuffer *output);
+    bool read_binary_data_v1(const Platform::ObjectBuffer &data, AppOptions_v1 *out_options);
+    void populate_default_data_v1(AppOptions_v1 *options);
 
     class OptionsManager
     {
+        std::vector<MathCore::vec2i> mainMonitorFullscreenResolutions;
+
     public:
+        MathCore::vec2i mainMonitorPosition;
+        MathCore::vec2i mainMonitorResolution;
         AppOptions_v1 currentOptions;
-        
+
+        void initializeDefaults();
+        bool loadOptionsFromBuffer(const Platform::ObjectBuffer &data);
+        void saveOptionsToBuffer(Platform::ObjectBuffer *output);
+
+        void checkSystemCompatibilityAfterLoad();
+
+        static OptionsManager *Instance();
+
+        std::vector<std::string> getGroups() const;
+        std::vector<std::string> getGroupKeys(const std::string &group) const;
+        std::vector<std::string> getGroupValuesForKey(const std::string &group, const std::string &key) const;
+        char *getGroupValueSelectedForKey(const std::string &group, const std::string &key);
+        const char *getGroupValueSelectedForKey(const std::string &group, const std::string &key) const;
+        bool setGroupValueSelectedForKey(const std::string &group, const std::string &key, const std::string &value);
     };
 
 }
