4d5db8d319fc7861d1f3163d2ed3d45bff7b7613
Alessandro Ribeiro <alessandro.ribeiro.silva@gmail.com>
2025-11-09 16:32:05 -0300

    Add RectangleCorner class and enhance stroke handling in ComponentRectangle
    - Introduced RectangleCorner class to manage corner radius and offsets.
    - Updated ComponentRectangle to utilize RectangleCorner for stroke calculations.
    - Added StrokeModeNone to handle stroke offset scenarios.
    - Refactored draw and stroke methods for improved clarity and functionality.

--- libs/InteractiveToolkit-AppKit/opengl/appkit-gl-engine/include/appkit-gl-engine/shaders/WithMask/MaskCommon.h
+++ libs/InteractiveToolkit-AppKit/opengl/appkit-gl-engine/include/appkit-gl-engine/shaders/WithMask/MaskCommon.h
@@ -14,62 +14,62 @@ namespace AppKit
 #define MASKSHADER_COMPUTE_MASK_FUNCTION                                                                                          \
     "float compute_mask(){\n"                                                                                                     \
     "  vec2 ptn_local_space = (uTransformToMask * vec4(gl_FragCoord.x, gl_FragCoord.y, (gl_FragCoord.z - 0.5) * 2.0, 1.0)).xy;\n" \
-    "  vec2 ptn_step = step(vec2(0.0), ptn_local_space);\n"                                                                       \
-    "  int quadrant = int((1.0 - ptn_step.x) * ( 2.0 + ptn_step.y ) + ptn_step.x * (1.0 - ptn_step.y) + 0.5);\n"                  \
-    "  vec2 quadrant_sign = sign(ptn_step - 0.5);\n"                                                                              \
-    "  ptn_local_space *= quadrant_sign;\n"                                                                                       \
-    "  vec2 corner_abs_space = abs(uMask_corner[quadrant]);\n"                                                                    \
-    "  vec2 size = corner_abs_space + vec2(uMask_radius[quadrant]);\n"                                                            \
-    "  vec2 ptn_on_proj_vec = ptn_local_space - corner_abs_space;\n"                                                              \
-    "  float valid = 1.0;\n" /* Branchless version */                                                                             \
-    "  float in_x_region = step(ptn_local_space.x, corner_abs_space.x);\n"                                                        \
-    "  float in_y_region = step(ptn_local_space.y, corner_abs_space.y);\n"                                                        \
-    "  float in_corner_region = (1.0 - in_x_region) * (1.0 - in_y_region);\n"                                                     \
-    "  float case1_valid = step(ptn_local_space.y, size.y);\n"                                                                    \
-    "  float case2_valid = step(ptn_local_space.x, size.x);\n"                                                                    \
-    "  float case3_valid = step(length(ptn_on_proj_vec), uMask_radius[quadrant]);\n"                                              \
-    "  valid *= mix(\n"                                                                                                           \
-    "    mix(case3_valid, case2_valid, in_y_region * (1.0 - in_x_region)),\n"                                                     \
-    "    case1_valid,\n"                                                                                                          \
-    "    in_x_region\n"                                                                                                           \
-    "  );\n"                                                                                                                      \
+    "  vec2 box_min = uMask_corner[2] - uMask_radius[2];\n"                                                                       \
+    "  vec2 box_max = uMask_corner[0] + uMask_radius[0];\n"                                                                       \
+    "  vec2 box_size_half = (box_max - box_min) * 0.5;\n"                                                                         \
+    "  vec2 box_center = (box_min + box_max) * 0.5;\n"                                                                            \
+    "  vec2 box_pt_offset = abs( ptn_local_space - box_center );\n"                                                               \
+    "  vec2 box_valid = step(box_pt_offset, box_size_half);\n"                                                                    \
+    "  float valid = box_valid.x * box_valid.y;\n"                                                                                \
+    "  vec2 qdrant_sign[4];\n"                                                                                                    \
+    "  qdrant_sign[0] = vec2( 1.0,  1.0);\n"                                                                                      \
+    "  qdrant_sign[1] = vec2( 1.0, -1.0);\n"                                                                                      \
+    "  qdrant_sign[2] = vec2(-1.0, -1.0);\n"                                                                                      \
+    "  qdrant_sign[3] = vec2(-1.0,  1.0);\n"                                                                                      \
+    "  for(int i = 0;i < 4;i++){\n"                                                                                               \
+    "    vec2 item_offset = ptn_local_space - uMask_corner[i];\n"                                                                 \
+    "    vec2 item_qdrant = step(item_offset * qdrant_sign[i], vec2( 0.0 ));\n"                                                   \
+    "    float item_qdrant_valid = max(item_qdrant.x, item_qdrant.y);\n"                                                          \
+    "    float item_valid = step(dot(item_offset,item_offset), uMask_radius[i]*uMask_radius[i]);\n"                               \
+    "    valid *= max(item_qdrant_valid, item_valid);\n"                                                                          \
+    "  }\n"                                                                                                                       \
     "  return valid;\n"                                                                                                           \
     "}\n"
 
-#define MASKSHADER_COMPUTE_MASK_FUNCTION_SMOOTH                                                                                            \
-    "float compute_mask(float margin){\n"                                                                                                  \
-    "  vec2 ptn_local_space = (uTransformToMask * vec4(gl_FragCoord.x, gl_FragCoord.y, (gl_FragCoord.z - 0.5) * 2.0, 1.0)).xy;\n"          \
-    "  vec2 ptn_step = step(vec2(0.0), ptn_local_space);\n"                                                                                \
-    "  int quadrant = int((1.0 - ptn_step.x) * ( 2.0 + ptn_step.y ) + ptn_step.x * (1.0 - ptn_step.y) + 0.5);\n"                           \
-    "  vec2 quadrant_sign = sign(ptn_step - 0.5);\n"                                                                                       \
-    "  ptn_local_space *= quadrant_sign;\n"                                                                                                \
-    "  vec2 corner_abs_space = abs(uMask_corner[quadrant]);\n"                                                                             \
-    "  vec2 size = corner_abs_space + vec2(uMask_radius[quadrant]);\n"                                                                     \
-    "  vec2 ptn_on_proj_vec = ptn_local_space - corner_abs_space;\n"                                                                       \
-    "  float valid = 1.0;\n" /* Branchless version */                                                                                      \
-    "  float in_x_region = step(ptn_local_space.x, corner_abs_space.x);\n"                                                                 \
-    "  float in_y_region = step(ptn_local_space.y, corner_abs_space.y);\n"                                                                 \
-    "  float in_corner_region = (1.0 - in_x_region) * (1.0 - in_y_region);\n"                                                              \
-    "  vec2 size_smooth = max(size - margin, vec2(0.0));\n"                                                                                \
-    "  float case1_valid = smoothstep(size.y, size_smooth.y, ptn_local_space.y) * smoothstep(size.x, size_smooth.x, ptn_local_space.x);\n" \
-    "  float case2_valid = case1_valid;\n"                                                                                                 \
-    "  float length_smooth = length(ptn_on_proj_vec);\n"                                                                                   \
-    "  float case3_valid = smoothstep(length_smooth, length_smooth + margin,uMask_radius[quadrant]);\n"                                    \
-    "  valid *= mix(\n"                                                                                                                    \
-    "    mix(case3_valid, case2_valid, in_y_region * (1.0 - in_x_region)),\n"                                                              \
-    "    case1_valid,\n"                                                                                                                   \
-    "    in_x_region\n"                                                                                                                    \
-    "  );\n"                                                                                                                               \
-    "  return valid;\n"                                                                                                                    \
-    "}\n"
-        // with branch implementation
-        // "  if (ptn_local_space.x < corner_abs_space.x) {\n" \
-        // "    valid *= step(ptn_local_space.y, size.y);\n" \
-        // "  } else if (ptn_local_space.y < corner_abs_space.y) {\n" \
-        // "    valid *= step(ptn_local_space.x, size.x);\n" \
-        // "  } else {\n" \
-        // "    valid *= step(length(ptn_on_proj_vec), uMask_radius[quadrant]);\n" \
-        // "  }\n" \
+// #define MASKSHADER_COMPUTE_MASK_FUNCTION_SMOOTH                                                                                            \
+//     "float compute_mask(float margin){\n"                                                                                                  \
+//     "  vec2 ptn_local_space = (uTransformToMask * vec4(gl_FragCoord.x, gl_FragCoord.y, (gl_FragCoord.z - 0.5) * 2.0, 1.0)).xy;\n"          \
+//     "  vec2 ptn_step = step(vec2(0.0), ptn_local_space);\n"                                                                                \
+//     "  int quadrant = int((1.0 - ptn_step.x) * ( 2.0 + ptn_step.y ) + ptn_step.x * (1.0 - ptn_step.y) + 0.5);\n"                           \
+//     "  vec2 quadrant_sign = sign(ptn_step - 0.5);\n"                                                                                       \
+//     "  ptn_local_space *= quadrant_sign;\n"                                                                                                \
+//     "  vec2 corner_abs_space = abs(uMask_corner[quadrant]);\n"                                                                             \
+//     "  vec2 size = corner_abs_space + vec2(uMask_radius[quadrant]);\n"                                                                     \
+//     "  vec2 ptn_on_proj_vec = ptn_local_space - corner_abs_space;\n"                                                                       \
+//     "  float valid = 1.0;\n" /* Branchless version */                                                                                      \
+//     "  float in_x_region = step(ptn_local_space.x, corner_abs_space.x);\n"                                                                 \
+//     "  float in_y_region = step(ptn_local_space.y, corner_abs_space.y);\n"                                                                 \
+//     "  float in_corner_region = (1.0 - in_x_region) * (1.0 - in_y_region);\n"                                                              \
+//     "  vec2 size_smooth = max(size - margin, vec2(0.0));\n"                                                                                \
+//     "  float case1_valid = smoothstep(size.y, size_smooth.y, ptn_local_space.y) * smoothstep(size.x, size_smooth.x, ptn_local_space.x);\n" \
+//     "  float case2_valid = case1_valid;\n"                                                                                                 \
+//     "  float length_smooth = length(ptn_on_proj_vec);\n"                                                                                   \
+//     "  float case3_valid = smoothstep(length_smooth, length_smooth + margin,uMask_radius[quadrant]);\n"                                    \
+//     "  valid *= mix(\n"                                                                                                                    \
+//     "    mix(case3_valid, case2_valid, in_y_region * (1.0 - in_x_region)),\n"                                                              \
+//     "    case1_valid,\n"                                                                                                                   \
+//     "    in_x_region\n"                                                                                                                    \
+//     "  );\n"                                                                                                                               \
+//     "  return valid;\n"                                                                                                                    \
+//     "}\n"
+//         // with branch implementation
+//         // "  if (ptn_local_space.x < corner_abs_space.x) {\n" \
+//         // "    valid *= step(ptn_local_space.y, size.y);\n" \
+//         // "  } else if (ptn_local_space.y < corner_abs_space.y) {\n" \
+//         // "    valid *= step(ptn_local_space.x, size.x);\n" \
+//         // "  } else {\n" \
+//         // "    valid *= step(length(ptn_on_proj_vec), uMask_radius[quadrant]);\n" \
+//         // "  }\n" \
 
 
         class AddShaderRectangleMask : public DefaultEngineShader
---
+++ libs/InteractiveToolkit-AppKit/opengl/appkit-gl-engine/src/Components/2d/RectangleCorner.h
@@ -0,0 +1,113 @@
+#pragma once
+
+#include <InteractiveToolkit/MathCore/MathCore.h>
+
+namespace AppKit
+{
+    namespace GLEngine
+    {
+        namespace Components
+        {
+
+            enum Order
+            {
+                Order_topRight = 0,
+                Order_bottomRight,
+                Order_bottomLeft,
+                Order_topLeft,
+                Order_Count
+            };
+
+            class RectangleCorner
+            {
+            public:
+                MathCore::vec2f size;
+                MathCore::vec4f radius;
+
+                float start_deg[Order_Count];
+                float end_deg[Order_Count];
+                MathCore::vec2f rad_factor[Order_Count];
+                MathCore::vec2f size_factor[Order_Count];
+
+                MathCore::vec2f output_center[Order_Count];
+                MathCore::vec4f output_radius;
+                MathCore::vec4f output_radius_factor;
+
+                MathCore::vec2f output_size;
+
+                RectangleCorner()
+                {
+
+                    const float start_deg_[Order_Count] = {90, 0, -90, -180};
+                    const float end_deg_[Order_Count] = {0, -90, -180, -270};
+                    const MathCore::vec2f rad_factor_[Order_Count] = {MathCore::vec2f(-1, -1), MathCore::vec2f(-1, 1), MathCore::vec2f(1, 1), MathCore::vec2f(1, -1)};
+                    const MathCore::vec2f size_factor_[Order_Count] = {MathCore::vec2f(1, 1), MathCore::vec2f(1, -1), MathCore::vec2f(-1, -1), MathCore::vec2f(-1, 1)};
+
+                    for (int i = 0; i < Order_Count; i++)
+                    {
+                        start_deg[i] = start_deg_[i];
+                        end_deg[i] = end_deg_[i];
+                        rad_factor[i] = rad_factor_[i];
+                        size_factor[i] = size_factor_[i];
+                    }
+                }
+
+                void setSize(const MathCore::vec2f &size)
+                {
+                    this->size = size;
+                }
+
+                void setRadius(const MathCore::vec4f &radius)
+                {
+                    this->radius = radius;
+                }
+
+                void applyOffset(float stroke_offset)
+                {
+                    output_size = size + stroke_offset * 2.0f;
+                    output_size = MathCore::OP<MathCore::vec2f>::maximum(0.0f, output_size);
+
+                    output_radius = radius + stroke_offset;
+                    output_radius = MathCore::OP<MathCore::vec4f>::maximum(0.0f, output_radius);
+
+                    float rad_r_y_factor = (output_size.height) / (output_radius[Order_topRight] + output_radius[Order_bottomRight]);
+                    float rad_b_x_factor = (output_size.width) / (output_radius[Order_bottomRight] + output_radius[Order_bottomLeft]);
+                    float rad_l_y_factor = (output_size.height) / (output_radius[Order_bottomLeft] + output_radius[Order_topLeft]);
+                    float rad_t_x_factor = (output_size.width) / (output_radius[Order_topLeft] + output_radius[Order_topRight]);
+
+                    
+                    output_radius_factor[Order_topRight] = MathCore::OP<float>::minimum(rad_t_x_factor, rad_r_y_factor);
+                    output_radius_factor[Order_bottomRight] = MathCore::OP<float>::minimum(rad_r_y_factor, rad_b_x_factor);
+                    output_radius_factor[Order_bottomLeft] = MathCore::OP<float>::minimum(rad_b_x_factor, rad_l_y_factor);
+                    output_radius_factor[Order_topLeft] = MathCore::OP<float>::minimum(rad_l_y_factor, rad_t_x_factor);
+
+                    output_radius_factor = MathCore::OP<MathCore::vec4f>::clamp(output_radius_factor, 0.0f, 1.0f);
+
+                    output_radius = output_radius_factor * output_radius;
+
+                    auto size_half = output_size * 0.5f;
+                    for (int i = 0; i < Order_Count; i++) {
+                        if (radius[i] <= -0.5f) {
+                            output_radius[i] = 0.0f;
+                            output_radius_factor[i] = 1.0f;
+                        }
+                        output_center[i] = size_half * size_factor[i] - output_radius[i] * size_factor[i];
+                    }
+                }
+
+                static MathCore::vec4u computeSegmentCounts(const MathCore::vec4f &radius, uint32_t segment_count_ref, float base_radius = 64.0f)
+                {
+                    float segment_factor = (float)segment_count_ref / base_radius;
+                    MathCore::vec4f radius_segment_count_f = radius * segment_factor;
+                    radius_segment_count_f = MathCore::OP<MathCore::vec4f>::ceil(radius_segment_count_f) + 0.5f;
+                    MathCore::vec4u radius_segment_count_i = (MathCore::vec4u)radius_segment_count_f;
+                    // for (int i = 0; i < 4; i++)
+                    //     if (radius_segment_count_i[i] == 0 && radius[i] == 0.0f)
+                    //         radius_segment_count_i[i] = segment_count_ref / 2;
+                    return radius_segment_count_i;
+                }
+            };
+
+        }
+    }
+}
