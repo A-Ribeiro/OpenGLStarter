c2b533b879b3b4f8cb8d8c9896ed518a51a12ead
Alessandro Ribeiro <alessandro.ribeiro.silva@gmail.com>
2025-12-09 15:05:49 -0300

    Refactor RichMessageTokenizer to enhance token parsing and add internal tag handling

--- src/example-ui/ui/util/RichMessageTokenizer.h
+++ src/example-ui/ui/util/RichMessageTokenizer.h
@@ -12,14 +12,43 @@ namespace AppKit
             char32_t separator_char;
             char32_t last_char_input;
 
-            //std::string rich_message;
+            bool is_internal_tag(const std::string &str) const;
+            std::string internalNextChar(bool *ended);
 
         public:
+            RichMessageTokenizer() = default;
             RichMessageTokenizer(const std::string &rich_message, char32_t separator_char = U' ');
 
-            std::string nextToken(bool *ended);
-            void nextLine(std::vector<std::string> *line_output, bool *ended);
+            //assignment operator
+            RichMessageTokenizer &operator=(const RichMessageTokenizer &other)
+            {
+                if (this != &other)
+                {
+                    this->generator = other.generator;
+                    this->rich_message_it = other.rich_message_it;
+                    this->rich_message_it_end = other.rich_message_it_end;
+                    this->separator_char = other.separator_char;
+                    this->last_char_input = other.last_char_input;
+                }
+                return *this;
+            }
+            // Move assignment operator
+            RichMessageTokenizer &operator=(RichMessageTokenizer &&other) noexcept
+            {
+                if (this != &other)
+                {
+                    this->generator = std::move(other.generator);
+                    this->rich_message_it = std::move(other.rich_message_it);
+                    this->rich_message_it_end = std::move(other.rich_message_it_end);
+                    this->separator_char = other.separator_char;
+                    this->last_char_input = other.last_char_input;
+                }
+                return *this;
+            }
 
+            std::string nextChar(bool *ended);
+            std::string nextWord(bool *ended);
+            void nextLine(std::vector<std::string> *line_output, bool *ended);
         };
     }
 }
