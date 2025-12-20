f8fe050d2d20e13aa0360ee207a1c3daf7683e70
Alessandro Ribeiro <alessandro.ribeiro.silva@gmail.com>
2025-12-20 19:44:12 -0300

    Implement shader color handling for black bars in the draw method and expose getShaderColor function in Fade class

--- src/example-ui/App.cpp
+++ src/example-ui/App.cpp
@@ -134,39 +134,112 @@ void App::draw()
 
     screenRenderWindow->OnAfterGraphPrecompute(&time);
 
-
     // smart clear stage
     {
         GLRenderState *renderState = GLRenderState::Instance();
 
         MathCore::vec4f clearColor = renderState->ClearColor;
         AppKit::GLEngine::iRect viewport = renderState->Viewport;
-        
-        // renderState->Viewport = screenRenderWindow->getCameraViewport();
 
         renderState->ClearColor = MathCore::vec4f(0, 0, 0, 1);
-        //MathCore::vec2i windowSize = window->getSize();
-        //renderState->Viewport = AppKit::GLEngine::iRect(0, 0, windowSize.x, windowSize.y);
-        glDisable(GL_SCISSOR_TEST);
-        glClear(GL_COLOR_BUFFER_BIT);
+        
+        // glDisable(GL_SCISSOR_TEST);
+        // glClear(GL_COLOR_BUFFER_BIT);
 
         renderState->ClearColor = clearColor;
-        // renderState->Viewport = viewport;
 
         glEnable(GL_SCISSOR_TEST);
         glScissor(viewport.x,
-            viewport.y,
-            viewport.w,
-            viewport.h);
+                  viewport.y,
+                  viewport.w,
+                  viewport.h);
 
         glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
     }
-
 
     if (mainScene != nullptr)
         mainScene->draw();
 
     fade->draw();
+
+    // draw black bars
+    {
+
+        glDisable(GL_SCISSOR_TEST);
+
+        GLRenderState *renderState = GLRenderState::Instance();
+
+        AppKit::GLEngine::DepthTestType oldDepthTest = renderState->DepthTest;
+        bool oldDepthTestEnabled = renderState->DepthWrite;
+        AppKit::GLEngine::BlendModeType oldBlendMode = renderState->BlendMode;
+        AppKit::GLEngine::iRect viewport = renderState->Viewport;
+        MathCore::vec2i windowSize = window->getSize();
+        renderState->Viewport = AppKit::GLEngine::iRect(windowSize.x, windowSize.y);
+
+        MathCore::vec3f vertex[8] = {
+            MathCore::vec3f(0, 0, 0),
+            MathCore::vec3f(windowSize.x, 0, 0),
+            MathCore::vec3f(windowSize.x, windowSize.y, 0),
+            MathCore::vec3f(0, windowSize.y, 0),
+        
+            MathCore::vec3f(viewport.x, viewport.y, 0),
+            MathCore::vec3f(viewport.x + viewport.w, viewport.y, 0),
+            MathCore::vec3f(viewport.x + viewport.w, viewport.y + viewport.h, 0),
+            MathCore::vec3f(viewport.x, viewport.y + viewport.h, 0)
+        };
+        
+        /*
+        3     2
+          7 6
+          4 5
+        0     1
+        */
+        uint32_t indices[24] = {
+            0, 1, 5,
+            0, 5, 4,
+
+            1, 2, 6,
+            1, 6, 5,
+
+            2, 3, 7,
+            2, 7, 6,
+
+            3, 0, 4,
+            3, 4, 7
+        };
+
+        AppKit::OpenGL::GLShaderColor *shaderColor = fade->getShaderColor();
+
+        renderState->CurrentShader = shaderColor;
+        renderState->DepthTest = AppKit::GLEngine::DepthTestDisabled;
+        renderState->DepthWrite = false;
+        renderState->BlendMode = AppKit::GLEngine::BlendModeDisabled;
+
+        shaderColor->setMatrix(
+            MathCore::GEN<MathCore::mat4f>::projection_ortho_lh_negative_one(
+                0.0f,                // Left
+                (float)windowSize.x, // Right
+                0.0f,                // Bottom
+                (float)windowSize.y, // Top
+                -1.0f,               // ZNear
+                1.0f                 // ZFar
+                ));
+
+        shaderColor->setColor(MathCore::vec4f(0, 0, 0, 1));
+
+        OPENGL_CMD(glEnableVertexAttribArray(shaderColor->vPosition));
+        OPENGL_CMD(glVertexAttribPointer(shaderColor->vPosition, 3, GL_FLOAT, false, sizeof(MathCore::vec3f), &vertex[0]));
+        // OPENGL_CMD(glDrawArrays(GL_TRIANGLES, 0, 3));
+        OPENGL_CMD(glDrawElements(GL_TRIANGLES, (GLsizei)(sizeof(indices) / sizeof(indices[0])), GL_UNSIGNED_INT, &indices[0]));
+
+        OPENGL_CMD(glDisableVertexAttribArray(shaderColor->vPosition));
+
+        // renderstate->CurrentShader = oldShader;
+        renderState->DepthTest = oldDepthTest;
+        renderState->DepthWrite = oldDepthTestEnabled;
+        renderState->BlendMode = oldBlendMode;
+        renderState->Viewport = viewport;
+    }
 
     // if (Keyboard::isPressed(KeyCode::Escape))
     //     exitApp();
