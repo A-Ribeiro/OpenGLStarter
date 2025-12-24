1cb1c6786dd7e3a893d92d1232022622cc2c373d
Alessandro Ribeiro <alessandro.ribeiro.silva@gmail.com>
2025-12-24 18:33:14 -0300

    scratching gamescene

---
+++ src/example-ui/GameScene.cpp
@@ -0,0 +1,84 @@
+﻿#include "GameScene.h"
+#include "App.h"
+
+#include <appkit-gl-engine/Components/Core/ComponentCameraOrthographic.h>
+
+PlayerInputState::PlayerInputState(RawInputFromDevice input_device)
+{
+    this->input_device = input_device;
+    state = InputState_None;
+}
+
+// to load skybox, textures, cubemaps, 3DModels and setup materials
+void GameScene::loadResources()
+{
+}
+// to load the scene graph
+void GameScene::loadGraph()
+{
+    root = Transform::CreateShared();
+    root->affectComponentStart = true;
+
+    auto main_camera = root->addChild(Transform::CreateShared("Main Camera"));
+}
+// to bind the resources to the current graph
+void GameScene::bindResourcesToGraph()
+{
+    AppKit::GLEngine::Engine *engine = AppKit::GLEngine::Engine::Instance();
+    GLRenderState *renderState = GLRenderState::Instance();
+
+    std::shared_ptr<ComponentCameraOrthographic> componentCameraOrthographic;
+    {
+        auto mainCamera = root->findTransformByName("Main Camera");
+        camera = componentCameraOrthographic = mainCamera->addNewComponent<ComponentCameraOrthographic>();
+    }
+}
+
+// clear all loaded scene
+void GameScene::unloadAll()
+{
+    root = nullptr;
+    camera = nullptr;
+}
+
+GameScene *GameScene::currentInstance = nullptr;
+
+GameScene::GameScene(
+    App *app,
+    Platform::Time *_time,
+    AppKit::GLEngine::RenderPipeline *_renderPipeline,
+    AppKit::GLEngine::ResourceHelper *_resourceHelper,
+    AppKit::GLEngine::ResourceMap *_resourceMap,
+    std::shared_ptr<AppKit::GLEngine::RenderWindowRegion> renderWindow) : AppKit::GLEngine::SceneBase(_time, _renderPipeline, _resourceHelper, _resourceMap, renderWindow),
+                                                                          random32(ITKCommon::RandomDefinition<uint32_t>::randomSeed()),
+                                                                          mathRandom(&random32)
+{
+    this->app = app;
+    GameScene::currentInstance = this;
+}
+
+GameScene::~GameScene()
+{
+    unload();
+    GameScene::currentInstance = nullptr;
+}
+
+void GameScene::draw()
+{
+    auto engine = AppKit::GLEngine::Engine::Instance();
+    if (engine->sRGBCapable)
+        glDisable(GL_FRAMEBUFFER_SRGB);
+    GLRenderState *state = GLRenderState::Instance();
+    state->DepthTest = DepthTestDisabled;
+    renderPipeline->runSinglePassPipeline(resourceMap, root, camera, false, OrthographicFilter_UsingAABB, &app->threadPool);
+    if (engine->sRGBCapable)
+        glEnable(GL_FRAMEBUFFER_SRGB);
+}
+
+void GameScene::onCameraViewportUpdate(const MathCore::vec2i &viewport_size)
+{
+}
+
+void GameScene::update(Platform::Time *elapsed)
+{
+}
---
+++ src/example-ui/GameScene.h
@@ -0,0 +1,90 @@
+﻿#pragma once
+
+#include <appkit-gl-engine/AppBase/SceneBase.h>
+
+#include <InteractiveToolkit-Extension/image/PNG.h>
+#include <InteractiveToolkit/Platform/Platform.h>
+
+#include <appkit-gl-engine/util/SpriteAtlas.h>
+
+#include <InteractiveToolkit/EventCore/PressReleaseDetector.h>
+
+class App;
+
+enum RawInputFromDevice
+{
+    Joystick0,
+    Joystick1,
+    Joystick2,
+    Joystick3,
+};
+
+enum IntInputState
+{
+    InputState_None = 0,
+    InputState_Up = 1,
+    InputState_Down = 1 << 1,
+    InputState_Left = 1 << 2,
+    InputState_Right = 1 << 3,
+    InputState_Action = 1 << 4,
+    InputState_Jump = 1 << 5,
+};
+
+class PlayerInputState
+{
+public:
+    RawInputFromDevice input_device;
+
+    EventCore::PressReleaseDetector up;
+    EventCore::PressReleaseDetector down;
+    EventCore::PressReleaseDetector left;
+    EventCore::PressReleaseDetector right;
+
+    EventCore::PressReleaseDetector jump;
+    EventCore::PressReleaseDetector action;
+
+    IntInputState state;
+
+    PlayerInputState(RawInputFromDevice input_device);
+
+
+};
+
+class GameScene : public AppKit::GLEngine::SceneBase
+{
+protected:
+    // to load skybox, textures, cubemaps, 3DModels and setup materials
+    virtual void loadResources();
+    // to load the scene graph
+    virtual void loadGraph();
+    // to bind the resources to the current graph
+    virtual void bindResourcesToGraph();
+
+    // clear all loaded scene
+    virtual void unloadAll();
+
+public:
+    static GameScene *currentInstance;
+
+    App *app;
+
+    ITKCommon::Random32 random32;
+    MathCore::MathRandomExt<ITKCommon::Random32> mathRandom;
+
+    std::vector<PlayerInputState> players;
+
+    GameScene(
+        App *app,
+        Platform::Time *_time,
+        AppKit::GLEngine::RenderPipeline *_renderPipeline,
+        AppKit::GLEngine::ResourceHelper *_resourceHelper,
+        AppKit::GLEngine::ResourceMap *_resourceMap,
+        std::shared_ptr<AppKit::GLEngine::RenderWindowRegion> renderWindow);
+    ~GameScene();
+
+    virtual void draw();
+
+    void onCameraViewportUpdate(const MathCore::vec2i &viewport_size);
+
+    void update(Platform::Time *elapsed);
+};
