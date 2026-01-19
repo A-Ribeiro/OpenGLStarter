b050a5394ba3e9035aab8b8f71bd96bd2cf188c3
Alessandro Ribeiro <alessandro.ribeiro.silva@gmail.com>
2026-01-19 18:09:07 -0300

    a working jump algorithm

--- src/example-ui/components/ComponentPlayer.cpp
+++ src/example-ui/components/ComponentPlayer.cpp
@@ -24,7 +24,7 @@ namespace AppKit
                 return CollisionCore::Sphere<MathCore::vec3f>(position, Radius.c_val());
             }
 
-            ComponentPlayer::ComponentPlayer() : Component(ComponentPlayer::Type), inputState(RawInputFromDevice::Joystick0)
+            ComponentPlayer::ComponentPlayer() : Component(ComponentPlayer::Type), inputState(RawInputFromDevice::Keyboard)
             {
 
                 debugDrawEnabled = false;
@@ -37,7 +37,7 @@ namespace AppKit
 
                 jumpState.setMinJumpHeight(300.0f);   // Set desired jump height
                 jumpState.setMaxJumpHeight(500.0f);   // Set desired jump height
-                jumpState.setRisingVelocity(1200.0f); // Set constant rising velocity
+                jumpState.setRisingVelocity(12000.0f); // Set constant rising velocity
                 jumpState.computeConstants(-3000.0f); // gravity
             }
 
@@ -58,7 +58,7 @@ namespace AppKit
                     if (transform->findTransformByName("DebugDrawCircle", 1) != nullptr)
                         return; // already created
                     auto self_ref = this->self<ComponentPlayer>();
-                    app->executeOnMainThread.enqueue([self_ref, transform]()
+                    app->executeOnMainThread.enqueue([this, self_ref, transform]()
                                                      {
                         auto debugDrawTransform = transform->addChild(Transform::CreateShared("DebugDrawCircle"));
                         //debugDrawTransform->setLocalPosition(MathCore::vec3f(0,0,-1.0f));
@@ -80,6 +80,13 @@ namespace AppKit
                             self_ref->debugDrawColor, // color
                             32, // segment_count
                             MathCore::quatf() // rotation
+                        );
+
+                        line_mounter->addLine(
+                            MathCore::vec3f(0, jumpState.getMinJumpHeight() - inner_radius,0), // a
+                            MathCore::vec3f(inner_radius, jumpState.getMinJumpHeight() - inner_radius, 0), // b
+                            self_ref->debugDrawThickness, // thickness
+                            ui::colorFromHex("#FF0000FF") // color
                         );
 
                         self_ref->app->gameScene->printHierarchy(); });
@@ -112,6 +119,11 @@ namespace AppKit
                 acceleration = MathCore::vec3f(0.0f, 0.0f, 0.0f); // reset acceleration
                 acceleration += gravity_px_per_sec;
 
+                // avoid affect the velocity, and give the jumpstate the control of the y velocity
+                // if (jumpState.isJumping())
+                //     acceleration.y = 0;
+
+                // vec3 traveled_position_aux = HeightAndTime::IntegrateAcceleration(velocity, acceleration, time->deltaTime);
                 velocity += acceleration * time->deltaTime;
 
                 const float max_velocity = 5000.0f;
@@ -122,29 +134,8 @@ namespace AppKit
                 auto gameAreaShared = gameArea.lock();
                 if (gameAreaShared == nullptr)
                     return;
-                // {
-                //     auto stageArea = gameAreaShared->StageArea.c_val();
 
-                //     auto sphere = CollisionCore::Sphere<MathCore::vec3f>(position + velocity * time->deltaTime, Radius.c_val());
-                //     auto lower_limit_plane = CollisionCore::Plane<MathCore::vec3f>(
-                //         stageArea.min_box,       // point
-                //         MathCore::vec3f(0, 1, 0) // normal
-                //     );
-
-                //     MathCore::vec3f penetration;
-                //     if (CollisionCore::Plane<MathCore::vec3f>::sphereIntersectsPlane(sphere, lower_limit_plane, &penetration))
-                //     {
-                //         velocity.y = 0.0f;
-                //         jumpState.setState(JumpState::Grounded);
-                //     }
-                // }
-                jumpState.setJumpPressed(inputState.jump.pressed);
-                if (inputState.jump.down)
-                    jumpState.jump(&velocity.y);
-
-                jumpState.updateVelocity(&velocity.y, time->deltaTime);
-                if (velocity.y > 0)
-                    printf("JumpState: %s, Velocity Y: %.2f\n", JumpState::stateToString(jumpState.getState()), velocity.y);
+                jumpState.update(&velocity.y, time->deltaTime, gravity_px_per_sec.y, inputState.jump.pressed, &position.y);
 
                 float x = inputState.x_axis;
 
