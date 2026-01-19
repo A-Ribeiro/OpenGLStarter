238afe3d5aa0d7a1901b57dafd1748d542337629
Alessandro Ribeiro <alessandro.ribeiro.silva@gmail.com>
2026-01-18 22:49:41 -0300

    Enhance jump mechanics by adjusting rising velocity and adding gravity computation in JumpState

--- src/example-ui/components/util/JumpState.h
+++ src/example-ui/components/util/JumpState.h
@@ -51,6 +51,14 @@ private:
 
     float risingVelocity;
 
+    float time_to_reach_zero_velocity;
+    float time_advance;
+
+    float maxJumpHeightFinal;
+    float minJumpHeightFinal;
+
+    float initialRisingVelocity;
+
 public:
     JumpState() : currentState(Grounded), canDoubleJump(true), maxJumpHeight(0.0f),
                   currentJumpHeight(0.0f), jumpButtonHeld(false), risingVelocity(0.0f) {}
@@ -74,6 +82,41 @@ public:
     void setMaxJumpHeight(float value) { maxJumpHeight = value; }
     float getMaxJumpHeight() const { return maxJumpHeight; }
 
+    void computeConstants(float gravity)
+    {
+        // acceleration is linear and constant
+        time_to_reach_zero_velocity = MathCore::OP<float>::abs(risingVelocity / gravity);
+
+        // the velocity(d) is a quadratic function on time
+        // d = v0 * t + 0.5 * gravity * t^2
+        // y = x^2 0.5 g * 1/g^2 + x^2 * 1/g
+        float height_rising_max = risingVelocity * time_to_reach_zero_velocity + 0.5f * gravity * time_to_reach_zero_velocity * time_to_reach_zero_velocity;
+
+        maxJumpHeightFinal = maxJumpHeight - height_rising_max;
+        minJumpHeightFinal = minJumpHeight - height_rising_max;
+
+        initialRisingVelocity = risingVelocity;
+
+        if (minJumpHeightFinal < 0)
+        {
+            // need to decrease rising velocity according the height offset
+            // d = v0 * (v0/gravity) + 0.5 * gravity * (v0/gravity)^2
+
+            // x = ± (sqrt(d) 1 g 0.816/sqrt(g)) and 1.225 sqrt(g) != 0 and g !=0
+
+            float d = minJumpHeight;
+            
+            float sqrt_d = MathCore::OP<float>::sqrt(d);
+            float sqrt_g = MathCore::OP<float>::sqrt(MathCore::OP<float>::abs(gravity));
+            const float sqrt_2 = MathCore::OP<float>::sqrt(2.0f);
+
+            initialRisingVelocity = sqrt_d * sqrt_g * sqrt_2;
+
+            minJumpHeightFinal = 0;
+            maxJumpHeightFinal = maxJumpHeight - d;
+        }
+    }
+
     // when hits jump button down event occurs
     void jump(float *velocityY)
     {
@@ -84,6 +127,7 @@ public:
             canDoubleJump = true;
             jumpButtonHeld = true;
             currentJumpHeight = 0.0f;
+            time_advance = 0.0f;
         }
         else if (canDoubleJump && currentState == Falling)
         {
@@ -92,6 +136,7 @@ public:
             canDoubleJump = false;
             jumpButtonHeld = true;
             currentJumpHeight = 0.0f;
+            time_advance = 0.0f;
         }
     }
 
@@ -125,25 +170,24 @@ public:
 
         if (currentState == Rising)
         {
-            *velocityY = risingVelocity;
-            currentJumpHeight += risingVelocity * deltaTime;
+            *velocityY = initialRisingVelocity;
+            currentJumpHeight += initialRisingVelocity * deltaTime;
             printf("JumpState: %s, Velocity Y: %.2f, currentJumpHeight: %.2f\n", stateToString(currentState), *velocityY, currentJumpHeight);
-            if (currentJumpHeight >= minJumpHeight)
+            if (currentJumpHeight >= minJumpHeightFinal)
             {
-                if (currentJumpHeight >= maxJumpHeight || !jumpButtonHeld)
+                if (currentJumpHeight >= maxJumpHeightFinal || !jumpButtonHeld)
                     currentState = RisingButtonReleased;
             }
         }
         else if (currentState == DoubleJumping)
         {
-            *velocityY = risingVelocity;
-            currentJumpHeight += risingVelocity * deltaTime;
+            *velocityY = initialRisingVelocity;
+            currentJumpHeight += initialRisingVelocity * deltaTime;
             printf("JumpState: %s, Velocity Y: %.2f, currentJumpHeight: %.2f\n", stateToString(currentState), *velocityY, currentJumpHeight);
-            if (currentJumpHeight >= minJumpHeight)
+            if (currentJumpHeight >= minJumpHeightFinal)
             {
                 currentState = DoubleJumpingButtonReleased;
             }
-            
         }
     }
 };
