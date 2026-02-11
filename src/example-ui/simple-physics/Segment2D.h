2d16259898e6219861f62b0342af4afdc4549366
Alessandro Ribeiro <alessandro.ribeiro.silva@gmail.com>
2026-02-11 17:30:06 -0300

    added segment ops

--- src/example-ui/simple-physics/Segment2D.h
+++ src/example-ui/simple-physics/Segment2D.h
@@ -13,6 +13,14 @@ namespace SimplePhysics
 
         Segment2D();
         Segment2D(const MathCore::vec2f &a, const MathCore::vec2f &b);
+
+        MathCore::vec2f closestPoint(const MathCore::vec2f &p) const;
+
+        static MathCore::vec2f closestPointToSegment(const MathCore::vec2f &p, const MathCore::vec2f &a, const MathCore::vec2f &b);
+
+        static bool segmentsIntersect(const MathCore::vec2f &p0, const MathCore::vec2f &p1, const MathCore::vec2f &p2, const MathCore::vec2f &p3);
+
+
     };
 
 }
