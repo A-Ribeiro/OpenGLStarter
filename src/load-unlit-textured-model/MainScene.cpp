282eaa79c0cb93a6ff8f04695c7113ccc531936e
Alessandro Ribeiro <alessandro.ribeiro.silva@gmail.com>
2026-06-04 15:40:56 -0300

    feat: Implement material creation and logging functions in MainScene

--- src/load-unlit-textured-model/MainScene.cpp
+++ src/load-unlit-textured-model/MainScene.cpp
@@ -25,11 +25,82 @@ namespace SmartImporter
         std::unordered_map<const ITKExtension::Model::Geometry *, bool> geometryProcessed;
         ResourceMap *resourceMap;
 
+        std::shared_ptr<Components::ComponentMaterial> createMaterial(const ITKExtension::Model::Material *mat)
+        {
+            // bool is_opaque = mat->is_opaque();
+            bool is_unlit = mat->is_unlit();
+            // bool is_two_sided = mat->is_two_sided();
+
+            if (is_unlit)
+                return createUnlitMaterial(mat);
+            else
+                return createPBRMaterial(mat);
+        }
+
+        /*
+        Material Float gltf.alphaCutoff: 0.500000
+    Material Float opacity: 1.000000
+    Material Float roughnessFactor: 1.000000
+    Material Float shininess: 0.000000
+    Material Float metallicFactor: 0.000000
+    Material Vec4 emissive: 0.000000, 0.000000, 0.000000, 1.000000
+    Material Vec4 base: 1.000000, 1.000000, 1.000000, 1.000000
+    Material Vec4 diffuse: 1.000000, 1.000000, 1.000000, 1.000000
+    Material Int gltf.unlit: 1
+    Material Int twosided: 0
+    Material Texture diffuse: stageBrick.png (Compatible with texture atlas: NO)
+    Material mat_stageBrick is opaque: YES
+         */
+
+        void printMaterialInfo(const ITKExtension::Model::Material *mat, int lvl)
+        {
+            for (const auto &kv : mat->stringValue)
+                printf("%*sMaterial String %s: %s\n", lvl * 2, "", kv.first.c_str(), kv.second.c_str());
+            for (const auto &kv : mat->floatValue)
+                printf("%*sMaterial Float %s: %f\n", lvl * 2, "", kv.first.c_str(), kv.second);
+            for (const auto &kv : mat->vec2Value)
+                printf("%*sMaterial Vec2 %s: %f, %f\n", lvl * 2, "", kv.first.c_str(), kv.second.x, kv.second.y);
+            for (const auto &kv : mat->vec3Value)
+                printf("%*sMaterial Vec3 %s: %f, %f, %f\n", lvl * 2, "", kv.first.c_str(), kv.second.x, kv.second.y, kv.second.z);
+            for (const auto &kv : mat->vec4Value)
+                printf("%*sMaterial Vec4 %s: %f, %f, %f, %f\n", lvl * 2, "", kv.first.c_str(), kv.second.x, kv.second.y, kv.second.z, kv.second.w);
+            for (const auto &kv : mat->intValue)
+                printf("%*sMaterial Int %s: %d\n", lvl * 2, "", kv.first.c_str(), kv.second);
+
+            for (const auto &tex : mat->textures)
+            {
+                // bool compatible_with_texture_atlas = geom->is_uv_compatible_with_texture_atlas(tex.uvIndex);
+                printf("%*sMaterial Texture %s: %s.%s\n", lvl * 2, "",
+                       TextureTypeToStr(tex.type),
+                       tex.filename.c_str(), tex.fileext.c_str());
+            }
+
+            bool is_opaque = mat->is_opaque();
+            bool is_unlit = mat->is_unlit();
+            bool is_two_sided = mat->is_two_sided();
+
+            printf("%*sMaterial %s is opaque: %s\n", lvl * 2, "", mat->name.c_str(), is_opaque ? "YES" : "NO");
+            printf("%*sMaterial %s is unlit: %s\n", lvl * 2, "", mat->name.c_str(), is_unlit ? "YES" : "NO");
+            printf("%*sMaterial %s is two-sided: %s\n", lvl * 2, "", mat->name.c_str(), is_two_sided ? "YES" : "NO");
+        }
+
         std::shared_ptr<Components::ComponentMaterial> createUnlitMaterial(const ITKExtension::Model::Material *mat)
         {
             using namespace AppKit::GLEngine::Components;
+
+            bool is_opaque = mat->is_opaque();
+            // bool is_unlit = mat->is_unlit();
+            // bool is_two_sided = mat->is_two_sided();
+
             auto material = Component::CreateShared<Components::ComponentMaterial>();
-            material->setShader(resourceMap->shaderUnlitTextureAlpha);
+            if (is_opaque)
+                material->setShader(resourceMap->shaderUnlitTexture);
+            else
+                material->setShader(resourceMap->shaderUnlitTextureAlpha);
+
+            auto diffuse = mat->vec4Value.find("diffuse");
+            if (diffuse != mat->vec4Value.end())
+                material->property_bag.getProperty("uColor").set<MathCore::vec4f>(diffuse->second);
 
             // ShaderUnlitTextureAlpha Property Bag:
 
@@ -48,8 +119,29 @@ namespace SmartImporter
         std::shared_ptr<Components::ComponentMaterial> createPBRMaterial(const ITKExtension::Model::Material *mat)
         {
             using namespace AppKit::GLEngine::Components;
+
+            bool is_opaque = mat->is_opaque();
+            // bool is_unlit = mat->is_unlit();
+            // bool is_two_sided = mat->is_two_sided();
+
             auto material = Component::CreateShared<Components::ComponentMaterial>();
             material->setShader(resourceMap->pbrShaderSelector);
+
+            auto diffuse = mat->vec4Value.find("diffuse");
+            if (diffuse != mat->vec4Value.end())
+                material->property_bag.getProperty("albedoColor").set<MathCore::vec3f>(CVT<vec4f>::toVec3(diffuse->second));
+
+            auto emissive = mat->vec4Value.find("emissive");
+            if (emissive != mat->vec4Value.end())
+                material->property_bag.getProperty("emissionColor").set<MathCore::vec3f>(CVT<vec4f>::toVec3(emissive->second));
+
+            auto roughness = mat->floatValue.find("roughnessFactor");
+            if (roughness != mat->floatValue.end())
+                material->property_bag.getProperty("roughness").set<float>(roughness->second);
+
+            auto metallic = mat->floatValue.find("metallicFactor");
+            if (metallic != mat->floatValue.end())
+                material->property_bag.getProperty("metallic").set<float>(metallic->second);
 
             // PBRShaderSelector Property Bag:
 
@@ -71,6 +163,25 @@ namespace SmartImporter
             return material;
         }
 
+
+        std::shared_ptr<Components::ComponentMaterial> createLineMaterial(const ITKExtension::Model::Material *mat)
+        {
+            using namespace AppKit::GLEngine::Components;
+
+            // bool is_opaque = mat->is_opaque();
+            // bool is_unlit = mat->is_unlit();
+            // bool is_two_sided = mat->is_two_sided();
+
+            auto material = Component::CreateShared<Components::ComponentMaterial>();
+            material->setShader(resourceMap->lineShader);
+
+            auto diffuse = mat->vec4Value.find("diffuse");
+            if (diffuse != mat->vec4Value.end())
+                material->property_bag.getProperty("uColor").set<MathCore::vec4f>(diffuse->second);
+
+            return material;
+        }
+
         void traverse(const ITKExtension::Model::Node &node, int lvl = 0)
         {
             auto localPosition = node.getLocalPosition();
@@ -78,24 +189,6 @@ namespace SmartImporter
             auto localRotation = node.getLocalRotation();
 
             printf("%*s(%s)\n", lvl * 2, "+", node.name.c_str());
-            // printf("%*sLocalPosition %f, %f, %f\n",
-            //        lvl * 2, "",
-            //        localPosition.x,
-            //        localPosition.y,
-            //        localPosition.z);
-
-            // printf("%*sLocalScale %f, %f, %f\n",
-            //        lvl * 2, "",
-            //        localScale.x,
-            //        localScale.y,
-            //        localScale.z);
-
-            // printf("%*sLocalRotation %f, %f, %f, %f\n",
-            //        lvl * 2, "",
-            //        localRotation.x,
-            //        localRotation.y,
-            //        localRotation.z,
-            //        localRotation.w);
 
             for (uint32_t gidx : node.geometries)
             {
@@ -116,19 +209,9 @@ namespace SmartImporter
                 {
                     printf("%*sGeometry %s is a triangle mesh\n", lvl * 2, "", geom->name.c_str());
 
-                    for (const auto &kv : mat->stringValue)
-                        printf("%*sMaterial String %s: %s\n", lvl * 2, "", kv.first.c_str(), kv.second.c_str());
-                    for (const auto &kv : mat->floatValue)
-                        printf("%*sMaterial Float %s: %f\n", lvl * 2, "", kv.first.c_str(), kv.second);
-                    for (const auto &kv : mat->vec2Value)
-                        printf("%*sMaterial Vec2 %s: %f, %f\n", lvl * 2, "", kv.first.c_str(), kv.second.x, kv.second.y);
-                    for (const auto &kv : mat->vec3Value)
-                        printf("%*sMaterial Vec3 %s: %f, %f, %f\n", lvl * 2, "", kv.first.c_str(), kv.second.x, kv.second.y, kv.second.z);
-                    for (const auto &kv : mat->vec4Value)
-                        printf("%*sMaterial Vec4 %s: %f, %f, %f, %f\n", lvl * 2, "", kv.first.c_str(), kv.second.x, kv.second.y, kv.second.z, kv.second.w);
-                    for (const auto &kv : mat->intValue)
-                        printf("%*sMaterial Int %s: %d\n", lvl * 2, "", kv.first.c_str(), kv.second);
+                    printMaterialInfo(mat, lvl * 2);
 
+                    printf("\nChecking if geometry %s is compatible with texture atlas...\n\n", geom->name.c_str());
                     for (const auto &tex : mat->textures)
                     {
                         bool compatible_with_texture_atlas = geom->is_uv_compatible_with_texture_atlas(tex.uvIndex);
@@ -137,14 +220,6 @@ namespace SmartImporter
                                tex.filename.c_str(), tex.fileext.c_str(),
                                compatible_with_texture_atlas ? "YES" : "NO");
                     }
-
-                    bool is_opaque = mat->is_opaque();
-                    bool is_unlit = mat->is_unlit();
-                    bool is_two_sided = mat->is_two_sided();
-
-                    printf("%*sMaterial %s is opaque: %s\n", lvl * 2, "", mat->name.c_str(), is_opaque ? "YES" : "NO");
-                    printf("%*sMaterial %s is unlit: %s\n", lvl * 2, "", mat->name.c_str(), is_unlit ? "YES" : "NO");
-                    printf("%*sMaterial %s is two-sided: %s\n", lvl * 2, "", mat->name.c_str(), is_two_sided ? "YES" : "NO");
                 }
                 else
                 {
@@ -202,13 +277,13 @@ namespace Scenes
         // return Basof2ToResource::loadAndConvert(path.c_str(), resourceMap, defaultPBRMaterial, nullptr, model_dynamic_upload, model_static_upload);
 
         SmartImporter::ModelSmasher smasher;
-#if defined(__linux__)
+#if defined(__linux__) && 0
         const char *home = std::getenv("HOME");
         std::string inputPath = std::string(home ? home : "") + "/Documents/papercat/stages_gltf/stage3_04.bams";
         auto path = std::unique_ptr<char, decltype(&std::free)>(realpath(inputPath.c_str(), nullptr), &std::free);
-        smasher.load(path ? path.get() : inputPath.c_str());
+        smasher.load(path ? path.get() : inputPath.c_str(), resourceMap);
 #else
-        smasher.load("/mnt/d/shared/papercat/stages_gltf/stage3_04.bams");
+        smasher.load("/mnt/d/shared/papercat/stages_gltf/stage3_04.bams", resourceMap);
 #endif
     }
     // to load the scene graph
