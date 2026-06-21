2c4c80d1655c9fe6987c39ecb0932383977d3e3e
Alessandro Ribeiro <alessandro.ribeiro.silva@gmail.com>
2026-06-20 21:10:14 -0300

    feat: Add CMake configuration for Lua and LuaJIT integration with updated build scripts

---
+++ libs/libLua.cmake
@@ -0,0 +1,25 @@
+if (TARGET liblua OR TARGET libluajit)
+    return()
+endif()
+
+if( NOT ITK_LUA_MODE )
+    set( ITK_LUA_MODE USE_LUA CACHE STRING
+                "Choose the type of Lua implementation."
+                FORCE )
+endif()
+set_property(CACHE ITK_LUA_MODE PROPERTY STRINGS USE_LUA USE_LUA_JIT)
+
+
+if (ITK_LUA_MODE STREQUAL USE_LUA_JIT)
+    if (NOT TARGET libluajit)
+        add_subdirectory("${CMAKE_CURRENT_LIST_DIR}/LuaJIT" "${CMAKE_BINARY_DIR}/lib/LuaJIT")
+        # alias for LuaJIT
+        add_library(liblua ALIAS libluajit)
+    endif()
+else()
+    if (NOT TARGET liblua)
+        add_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Lua" "${CMAKE_BINARY_DIR}/lib/Lua")
+    endif()
+endif()
+
+message(STATUS "[ITK_LUA_MODE   ${ITK_LUA_MODE}]")
