10ab6d95b5331697c390c178f43e6e2e42e37b15
Alessandro Ribeiro <alessandro.ribeiro.silva@gmail.com>
2026-06-26 11:28:10 -0300

    feat: Add clear method and estimateGeneratedAtlasCount to SpriteAtlasGenerator for improved atlas management

--- libs/InteractiveToolkit-AppKit/opengl/appkit-gl-engine/src/util/SpriteAtlas.cpp
+++ libs/InteractiveToolkit-AppKit/opengl/appkit-gl-engine/src/util/SpriteAtlas.cpp
@@ -59,6 +59,37 @@ namespace AppKit
                 throw std::runtime_error("Entry not found: " + name);
         }
 
+        void SpriteAtlasGenerator::clear()
+        {
+            entries.clear();
+        }
+
+        struct ImageBuffer
+        {
+            int w, h, channels, depth;
+            std::unique_ptr<char, void (*)(char *)> buffer;
+        };
+
+        std::shared_ptr<ImageBuffer> smartLoadImage(const char *path)
+        {
+            auto imageBuffer = std::make_shared<ImageBuffer>();
+
+            bool invertY = false;
+
+            if (ITKExtension::Image::PNG::isPNGFilename(path))
+                imageBuffer->buffer = std::unique_ptr<char, void (*)(char *)>(ITKExtension::Image::PNG::readPNG(path, &imageBuffer->w, &imageBuffer->h, &imageBuffer->channels, &imageBuffer->depth, invertY),
+                                                                              [](char *ptr)
+                                                                              { if(ptr) {char *aux = ptr;ITKExtension::Image::PNG::closePNG(aux); } });
+            else if (ITKExtension::Image::JPG::isJPGFilename(path))
+                imageBuffer->buffer = std::unique_ptr<char, void (*)(char *)>(ITKExtension::Image::JPG::readJPG(path, &imageBuffer->w, &imageBuffer->h, &imageBuffer->channels, &imageBuffer->depth, invertY),
+                                                                              [](char *ptr)
+                                                                              { if(ptr) {char *aux = ptr;ITKExtension::Image::JPG::closeJPG(aux); } });
+            if (!imageBuffer->buffer)
+                return nullptr;
+
+            return imageBuffer;
+        }
+
         std::vector<std::shared_ptr<SpriteAtlas>> SpriteAtlasGenerator::generateAtlas(const std::string &base_path,
                                                                                       const ResourceMap &resourceMap, bool sRGB, bool use_fast_positioning, int spaceBetweenSpites_px, int max_atlas_size)
         {
@@ -90,43 +121,20 @@ namespace AppKit
                 // auto path = resourceMap.dir.getBasePath() + genEntry.texture_path;
                 auto path = base_path + genEntry.texture_path;
 
-                int w, h, channels, depth;
+                // int w, h, channels, depth;
                 bool invertY = false;
 
-                bool isPNG = ITKExtension::Image::PNG::isPNGFilename(path.c_str());
-                bool isJPG = ITKExtension::Image::JPG::isJPGFilename(path.c_str());
+                auto imageBuffer = smartLoadImage(path.c_str());
+                if (!imageBuffer)
+                    throw std::runtime_error("Failed to load image for sprite '" + name + "': " + path);
 
-                void (*closeFnc)(char *&) = nullptr;
-
-                char *buffer = nullptr;
-                if (isPNG)
-                {
-                    buffer = ITKExtension::Image::PNG::readPNG(path.c_str(), &w, &h, &channels, &depth, invertY);
-                    closeFnc = &ITKExtension::Image::PNG::closePNG;
-                }
-                else if (isJPG)
-                {
-                    buffer = ITKExtension::Image::JPG::readJPG(path.c_str(), &w, &h, &channels, &depth, invertY);
-                    closeFnc = &ITKExtension::Image::JPG::closeJPG;
-                }
-
-                if (buffer == nullptr)
-                    throw std::runtime_error("Error loading texture: " + path);
-
-                EventCore::ExecuteOnScopeEnd closeBuffer(
-                    [&buffer, closeFnc]()
-                    {
-                        if (closeFnc)
-                            closeFnc(buffer);
-                    });
-
-                if ((channels != 4 && channels != 3 && channels != 1) || depth != 8)
+                if ((imageBuffer->channels != 4 && imageBuffer->channels != 3 && imageBuffer->channels != 1) || imageBuffer->depth != 8)
                     throw std::runtime_error("Invalid image format for sprite '" + name + "': expected Gray, RGB, RGBA 8-bit");
 
                 if (channel_used == -1)
-                    channel_used = channels;
+                    channel_used = imageBuffer->channels;
 
-                auto atlasElementFace = atlas->addElement(name, w, h);
+                auto atlasElementFace = atlas->addElement(name, imageBuffer->w, imageBuffer->h);
 
                 atlas->organizePositions(use_fast_positioning);
                 if (combinedEntries.size() > 0 && (atlas->textureResolution.w > max_atlas_size || atlas->textureResolution.h > max_atlas_size))
@@ -138,32 +146,19 @@ namespace AppKit
                     if (channel_used == 4)
                     {
                         auto rgba = atlas->createRGBA();
-                        result_single->texture->uploadBufferRGBA_8888(
-                            (const void *)rgba.get(),
-                            atlas->textureResolution.w,
-                            atlas->textureResolution.h,
-                            sRGB);
-                        // atlas.releaseRGBA(&rgba);
+                        result_single->texture->uploadBufferRGBA_8888((const void *)rgba.get(), atlas->textureResolution.w, atlas->textureResolution.h, sRGB);
                     }
                     else if (channel_used == 3)
                     {
                         auto rgba = atlas->createRGB();
-                        result_single->texture->uploadBufferRGB_888(
-                            (const void *)rgba.get(),
-                            atlas->textureResolution.w,
-                            atlas->textureResolution.h,
-                            sRGB);
-                        // atlas.releaseRGBA(&rgba);
+                        result_single->texture->uploadBufferRGB_888((const void *)rgba.get(), atlas->textureResolution.w, atlas->textureResolution.h, sRGB);
                     }
                     else if (channel_used == 1)
                     {
                         auto rgba = atlas->createGray();
-                        result_single->texture->uploadBufferRed8(
-                            (const void *)rgba.get(),
-                            atlas->textureResolution.w,
-                            atlas->textureResolution.h);
-                        // atlas.releaseRGBA(&rgba);
+                        result_single->texture->uploadBufferRed8((const void *)rgba.get(), atlas->textureResolution.w, atlas->textureResolution.h);
                     }
+
                     MathCore::vec2f atlasSize_inv = 1.0f / MathCore::vec2f(atlas->textureResolution.w, atlas->textureResolution.h);
                     for (const auto &item : combinedEntries)
                     {
@@ -182,17 +177,17 @@ namespace AppKit
 
                     result_single = std::make_shared<SpriteAtlas>();
                     result_single->texture = std::make_shared<AppKit::OpenGL::GLTexture>();
-                    atlasElementFace = atlas->addElement(name, w, h);
+                    atlasElementFace = atlas->addElement(name, imageBuffer->w, imageBuffer->h);
 
                     combinedEntries.clear();
                 }
 
-                if (channels == 4)
-                    atlasElementFace->copyFromRGBABuffer((uint8_t *)buffer, w * 4);
-                else if (channels == 3)
-                    atlasElementFace->copyFromRGBBuffer((uint8_t *)buffer);
-                else if (channels == 1)
-                    atlasElementFace->copyFromGrayBuffer((uint8_t *)buffer);
+                if (imageBuffer->channels == 4)
+                    atlasElementFace->copyFromRGBABuffer((uint8_t *)imageBuffer->buffer.get(), imageBuffer->w * 4);
+                else if (imageBuffer->channels == 3)
+                    atlasElementFace->copyFromRGBBuffer((uint8_t *)imageBuffer->buffer.get());
+                else if (imageBuffer->channels == 1)
+                    atlasElementFace->copyFromGrayBuffer((uint8_t *)imageBuffer->buffer.get());
 
                 combinedEntries.push_back({name, genEntry, atlasElementFace.get()});
             }
@@ -202,32 +197,19 @@ namespace AppKit
             if (channel_used == 4 || channel_used == -1)
             {
                 auto rgba = atlas->createRGBA();
-                result_single->texture->uploadBufferRGBA_8888(
-                    (const void *)rgba.get(),
-                    atlas->textureResolution.w,
-                    atlas->textureResolution.h,
-                    sRGB);
-                // atlas.releaseRGBA(&rgba);
+                result_single->texture->uploadBufferRGBA_8888((const void *)rgba.get(), atlas->textureResolution.w, atlas->textureResolution.h, sRGB);
             }
             else if (channel_used == 3)
             {
                 auto rgba = atlas->createRGB();
-                result_single->texture->uploadBufferRGB_888(
-                    (const void *)rgba.get(),
-                    atlas->textureResolution.w,
-                    atlas->textureResolution.h,
-                    sRGB);
-                // atlas.releaseRGBA(&rgba);
+                result_single->texture->uploadBufferRGB_888((const void *)rgba.get(), atlas->textureResolution.w, atlas->textureResolution.h, sRGB);
             }
             else if (channel_used == 1)
             {
                 auto rgba = atlas->createGray();
-                result_single->texture->uploadBufferRed8(
-                    (const void *)rgba.get(),
-                    atlas->textureResolution.w,
-                    atlas->textureResolution.h);
-                // atlas.releaseRGBA(&rgba);
+                result_single->texture->uploadBufferRed8((const void *)rgba.get(), atlas->textureResolution.w, atlas->textureResolution.h);
             }
+
             MathCore::vec2f atlasSize_inv = 1.0f / MathCore::vec2f(atlas->textureResolution.w, atlas->textureResolution.h);
             for (const auto &item : combinedEntries)
             {
@@ -243,6 +225,77 @@ namespace AppKit
             result_array.push_back(result_single);
 
             return result_array;
+        }
+
+        int SpriteAtlasGenerator::estimateGeneratedAtlasCount(
+            const std::string &base_path,
+            const ResourceMap &resourceMap,
+            bool sRGB,
+            bool use_fast_positioning,
+            int spaceBetweenSpites_px,
+            int max_atlas_size)
+        {
+            std::vector<std::shared_ptr<SpriteAtlas>> result_array;
+
+            if (entries.empty())
+                return 0;
+
+            int count_atlas = 0;
+
+            std::unique_ptr<ITKExtension::Atlas::Atlas> atlas = STL_Tools::make_unique<ITKExtension::Atlas::Atlas>(spaceBetweenSpites_px, spaceBetweenSpites_px);
+
+            struct Combined_entry
+            {
+                const std::string &name;
+                const GeneratorEntry &genEntry;
+                ITKExtension::Atlas::AtlasElement *atlasElementFace;
+            };
+            std::vector<Combined_entry> combinedEntries;
+
+            int channel_used = -1;
+
+            for (const auto &entry : entries)
+            {
+                const std::string &name = entry.first;
+                const GeneratorEntry &genEntry = entry.second;
+
+                auto path = base_path + genEntry.texture_path;
+
+                bool invertY = false;
+
+                auto imageBuffer = smartLoadImage(path.c_str());
+                if (!imageBuffer)
+                    throw std::runtime_error("Failed to load image for sprite '" + name + "': " + path);
+
+                if ((imageBuffer->channels != 4 && imageBuffer->channels != 3 && imageBuffer->channels != 1) || imageBuffer->depth != 8)
+                    throw std::runtime_error("Invalid image format for sprite '" + name + "': expected Gray, RGB, RGBA 8-bit");
+
+                if (channel_used == -1)
+                    channel_used = imageBuffer->channels;
+
+                auto atlasElementFace = atlas->addElement(name, imageBuffer->w, imageBuffer->h);
+
+                atlas->organizePositions(use_fast_positioning);
+                if (combinedEntries.size() > 0 && (atlas->textureResolution.w > max_atlas_size || atlas->textureResolution.h > max_atlas_size))
+                {
+                    // flush atlas and start a new one
+                    atlas->removeLastInsertedElement();
+                    atlas->organizePositions(use_fast_positioning);
+                    count_atlas++;
+
+                    atlas = STL_Tools::make_unique<ITKExtension::Atlas::Atlas>(spaceBetweenSpites_px, spaceBetweenSpites_px);
+
+                    atlasElementFace = atlas->addElement(name, imageBuffer->w, imageBuffer->h);
+                    combinedEntries.clear();
+                }
+
+                combinedEntries.push_back({name, genEntry, atlasElementFace.get()});
+            }
+
+            atlas->organizePositions(use_fast_positioning);
+            count_atlas++;
+
+            return count_atlas;
         }
 
     }
